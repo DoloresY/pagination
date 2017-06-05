4f1cd4182021a60cc006c9cb2d6ba42397a4d543
Dolores <yaoli199@163.com>
2017-06-05 21:39:34 +0800

    扩展性优化

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 # pagination
 ### 自定义分页插件
 
-此控件依赖jquery,包小而简，支持用户自定义配置
+#### 说明
+1. pagination.min.js是压缩后的核心代码, pagination.js是开发源代码
+2. 将pagination.min.js引入项目中，可自定义样式，参考index.html中script配置分页参数
 
-<pre><code> 
-+  pageNo: 1,		   //可配置当前页 
-+  pageSize: 1,	   //当前页显示条数 
-+  pageStep: 10,   //当前可见最多页码个数 
-+  debug: true,	   //调试true，实际false, 
-+  url: "http://127.0.0.1:8020/pagination/test.json", //调用接口url
-+  insert: function(data){   //可配置页面插入内容
-    var content = "";
-    for(var i = 0; i < data.length; i++ ){
-      content += "&lt;p>"+data[i].id+" : "+data[i].title+"&lt;/p>"
-    }
-    $(".text").html(content);
+#### 简要
+此插件目前依赖jquery开发，包小而简。具有优雅的内部代码，良好的性能体验，且项目全开源，您可以任意获取开源代码进行查看，参考与学习，可以免费的将他用于你的各个项目中
+<pre>
+<code> 
+pageNo: 1,		         //可配置当前页 
+pageSize: 1,		         //当前页显示条数 
+pageStep: 10,		         //当前可见最多页码个数 
+debug: true,		         //调试true，实际false, ( 调试时请将项目放到本地站点下测试 )
+url: "test.json",		     //调用接口url
+insert: function(data){		     //可配置页面插入内容  data为接口返回数据  
+  var content = "";
+  for(var i = 0; i < data.length; i++ ){
+    content += "&lt;p>"+data[i].id+" : "+data[i].title+"&lt;/p>";
   }
-</code></pre>
+  $(".text").html(content);
+}
+</code>
+</pre>
+
+#### 备注
+此插件为个人开发，如有任何问题，欢迎随时提到邮箱yaoli199@163.com，谢谢！
--- js/pagination.js
+++ js/pagination.js
@@ -1,98 +1,103 @@
 /*
  * Descreiption: 分页插件
  * Email: yaoli199@163.com
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Dolores
  * Date: 2017-06-04
  */
-
-function pageTemplate(opts){
-	var optsDefault = {pageNo:1, pageSize:10};
-	opts = opts ? opts : optsDefault;
-	if(!opts.pageStep){
-		opts.pageStep = optsDefault.pageStep;
-	}
-	this.getList = function(){
-		ajaxData(opts);
-	}
-	function ajaxData(opts){
-		$.ajax({
-	        url: opts.url,
-	        data:{ "pageNo" : opts.pageNo,"pageSize": opts.pageSize },
-	        type:"get",
-	        dataType:"JSON",
-	        success: function(data){
-	        	var totalPage = opts.debug? (data.data.length % opts.pageSize ? (data.data.length / opts.pageSize)+1 : (data.data.length / opts.pageSize)):data.totalPage;
-	        	var data = data.data.slice((opts.pageNo-1)*opts.pageSize,opts.pageNo*opts.pageSize);
-	        	pagination(opts.pageNo,totalPage);
-	        	opts = opts.insert ? opts.insert(data) : insert(data);
-	        },error: function(data){
-				console.log(data);
-	        }
-	    })
-	}
-	function insert(data){
-		var content = "";
-		for(var i = 0; i < data.length; i++ ){
-			content += "<li>"+data[i].id+" : "+data[i].title+"</li>"
+(function($) {
+	$.fn.pageTemplate = function(opts){
+		var htmlTmp = "<div class='pageHtml'></div>";
+		var pageTmp = "<div id='pagination'></div>";
+		var _temp = htmlTmp + pageTmp
+		$(this).append(_temp);
+		var optsDefault = {pageNo:1, pageSize:10};
+		opts = opts ? opts : optsDefault;
+		if(!opts.pageStep){
+			opts.pageStep = optsDefault.pageStep;
 		}
-		$(".text").html(content);
-	}
-	function pagination(pageNo,totalPage){
-		var content = "";
-		if(totalPage > 1 ){
-			content += "<a"+(pageNo == 1? " class='page_gray'":" data-page='"+(pageNo-1)+"'")+" href='javascript:;'>&laquo;</a>";  //左箭头
-			for(var i = 1 ; i <= totalPage ; i++){
-				if(totalPage>=opts.pageStep){
-					var leftPage = pageNo - 1;
-					var rightPage = totalPage - pageNo;
-					if( leftPage > 10 && rightPage > 10 ){
-						if( i == 3 || i == (totalPage-2) ) {
-							content += "<span>...</span>";
-						}else if( i == 1 || i == 2 || i == totalPage || i == totalPage-1 || i == pageNo || i == pageNo-1 || i == pageNo+1){
-							content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
-						}
-					}else if( leftPage < rightPage ){
-						if( pageNo < 5 ){
-							if( i <= 5 || i == totalPage-1 || i == totalPage ){
-								content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
-							}else if( i == 6 ){
-								content += "<span>...</span>";
-							}
-						}else {
-							if( i == 1 || i == 2 || i == 3 || i == pageNo-1 || i == pageNo || i == pageNo+1 || i == totalPage-1 || i == totalPage ){
-								content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
-							}else if( i == 4 || i == totalPage-2 ){
-								content += "<span>...</span>";
-							}
-						}
-					}else if( leftPage >= rightPage ){
-						if( pageNo > totalPage - 4 ) {
-							if( i == 1 || i == 2 || i >= totalPage-4 ) {
-								content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
-							}else if( i == 3 ){
-								content += "<span>...</span>";
-							}
-						}else{
-							if( i == 1 || i == 2 || i == pageNo-1 || i == pageNo || i == pageNo+1 || i == totalPage-2 || i == totalPage-1 || i == totalPage+1 ){
-								content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
-							}else if( i == 3 || i == pageNo+2 ){
-								content += "<span>...</span>";
-							}
-						}
-					}
-				}else{
-					content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
-				}
+		return this.each(function() {
+	      	ajaxData(opts);
+	    });
+		function ajaxData(opts){
+			$.ajax({
+		        url: opts.url,
+		        data:{ "pageNo" : opts.pageNo,"pageSize": opts.pageSize },
+		        type:"get",
+		        dataType:"JSON",
+		        success: function(data){
+		        	var totalPage = opts.debug? (data.data.length % opts.pageSize ? (data.data.length / opts.pageSize)+1 : (data.data.length / opts.pageSize)):data.totalPage;
+		        	var data = data.data.slice((opts.pageNo-1)*opts.pageSize,opts.pageNo*opts.pageSize);
+		        	pagination(opts.pageNo,totalPage);
+		        	opts = opts.insert ? opts.insert(data) : insert(data);
+		        },error: function(data){
+					console.log(data);
+		        }
+		    })
+		}
+		function insert(data){	//插入html
+			var content = "";
+			for(var i = 0; i < data.length; i++ ){
+				content += "<li>"+data[i].id+" : "+data[i].title+"</li>"
 			}
-			content += "<a"+(pageNo == totalPage? " class='page_gray'":" data-page='"+(pageNo+1)+"'")+" href='javascript:;'>&raquo;</a>";  //右箭头
+			$(".pageHtml").html(content);
 		}
-		$("#pagination").html(content);
-		//click event
-		$("#pagination a[data-page]").on("click",function(){
-			pageNo = $(this).attr('data-page');
-			opts.pageNo = parseInt(pageNo);
-			ajaxData(opts);
-		})
+		function pagination(pageNo,totalPage){
+			var content = "";
+			if(totalPage > 1 ){
+				content += "<a"+(pageNo == 1? " class='page_gray'":" data-page='"+(pageNo-1)+"'")+" href='javascript:;'>&laquo;</a>";  //左箭头
+				for(var i = 1 ; i <= totalPage ; i++){
+					if(totalPage>=opts.pageStep){
+						var leftPage = pageNo - 1;
+						var rightPage = totalPage - pageNo;
+						if( leftPage > 10 && rightPage > 10 ){
+							if( i == 3 || i == (totalPage-2) ) {
+								content += "<span>...</span>";
+							}else if( i == 1 || i == 2 || i == totalPage || i == totalPage-1 || i == pageNo || i == pageNo-1 || i == pageNo+1){
+								content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
+							}
+						}else if( leftPage < rightPage ){
+							if( pageNo < 5 ){
+								if( i <= 5 || i == totalPage-1 || i == totalPage ){
+									content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
+								}else if( i == 6 ){
+									content += "<span>...</span>";
+								}
+							}else {
+								if( i == 1 || i == 2 || i == 3 || i == pageNo-1 || i == pageNo || i == pageNo+1 || i == totalPage-1 || i == totalPage ){
+									content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
+								}else if( i == 4 || i == totalPage-2 ){
+									content += "<span>...</span>";
+								}
+							}
+						}else if( leftPage >= rightPage ){
+							if( pageNo > totalPage - 4 ) {
+								if( i == 1 || i == 2 || i >= totalPage-4 ) {
+									content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
+								}else if( i == 3 ){
+									content += "<span>...</span>";
+								}
+							}else{
+								if( i == 1 || i == 2 || i == pageNo-1 || i == pageNo || i == pageNo+1 || i == totalPage-2 || i == totalPage-1 || i == totalPage+1 ){
+									content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
+								}else if( i == 3 || i == pageNo+2 ){
+									content += "<span>...</span>";
+								}
+							}
+						}
+					}else{
+						content += "<a "+(i==pageNo? "class='selected'":"data-page='"+i+"'")+" href='javascript:;'>"+i+"</a>";
+					}
+				}
+				content += "<a"+(pageNo == totalPage? " class='page_gray'":" data-page='"+(pageNo+1)+"'")+" href='javascript:;'>&raquo;</a>";  //右箭头
+			}
+			$("#pagination").html(content);
+			//click event
+			$("#pagination").children("a[data-page]").on("click",function(){
+				pageNo = $(this).attr('data-page');
+				opts.pageNo = parseInt(pageNo);
+				ajaxData(opts);
+			})
+		}
 	}
-}
+})(jQuery);
